4a7b5fa32612864136d836ff52b5bfb674f20cbc
mykola-b31 <babkokolya81@gmail.com>
2025-10-15 21:33:17 +0300

    Add UI

--- blog.php
+++ blog.php
@@ -1,40 +1,53 @@
 <?php include 'connections/my_site_db.php'; ?>
 <!DOCTYPE html>
-<html lang="en">
+<html lang="uk">
 <head>
     <meta charset="UTF-8">
-    <title>Blog</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Мій Блог</title>
+    <link rel="stylesheet" href="styles/style.css">
 </head>
 <body>
 <header>
-    <nav>
-        <a href="#">Увійти</a> |
-        <a href="#">Новий запис</a> |
-        <a href="#">Надіслати повідомлення</a> |
-        <a href="#">Фото</a> |
-        <a href="#">Файли</a> |
-        <a href="#">Адміністратору</a> |
-        <a href="inform.html">Інформація</a> |
-        <a href="#">Вийти</a>
-    </nav>
+    <div id="header_container">
+        <a href="blog.php" class="logo-text">Мій Блог</a>
+        <nav class="menu">
+            <ul class="menu_list">
+                <li class="menu_item"><a href="#" class="menu_link">Новий запис</a></li>
+                <li class="menu_item"><a href="#" class="menu_link">Надіслати повідомлення</a></li>
+                <li class="menu_item"><a href="#" class="menu_link">Фото</a></li>
+                <li class="menu_item"><a href="#" class="menu_link">Файли</a></li>
+                <li class="menu_item"><a href="#" class="menu_link">Адміністратору</a></li>
+                <li class="menu_item"><a href="inform.html" class="menu_link">Інформація</a></li>
+                <li class="menu_item"><a href="#" class="menu_link">Увійти</a></li>
+            </ul>
+        </nav>
+    </div>
 </header>
-<hr>
-<p>
-    <em>Вітаю вас у своєму блозі!<br><br>
-    На сторінках цього сайту я буду ділитися<br><br>
-    нотатками та записами зі своїх подорожей,<br><br>
-    а також іншини цікавими матеріалами!</em>
-</p>
-<hr>
-<?php
-$sql = "SELECT * FROM notes ORDER BY created DESC;";
-$select_note = mysqli_query($link, $sql);
-while ($note = mysqli_fetch_array($select_note)) {
-    echo "{$note['id']} ";
-    echo "<a href='comments.php?note={$note['id']}'>{$note['title']}</a><br>";
-    echo "{$note['created']}<br>";
-    echo "{$note['article']}<br><hr>";
-}
-?>
+
+<main>
+    <div class="main_content">
+        <div class="welcome-message">
+            <h2>Вітаю вас у своєму блозі!</h2>
+            <p>На сторінках цього сайту я буду ділитися нотатками та записами зі своїх подорожей, а також іншими цікавими матеріалами!</p>
+        </div>
+
+        <?php
+        $sql = "SELECT * FROM notes ORDER BY created DESC;";
+        $select_note = mysqli_query($link, $sql);
+        while ($note = mysqli_fetch_array($select_note)) {
+            echo "<article class='post-card'>";
+            echo "<h3 class='post-title'><a href='comments.php?note={$note['id']}'>{$note['title']}</a></h3>";
+            echo "<p class='post-meta'>Опубліковано: {$note['created']}</p>";
+            echo "<div class='post-article'>{$note['article']}</div>";
+            echo "</article>";
+        }
+        ?>
+    </div>
+</main>
+
+<footer>
+    <p>&copy; <?php echo date("Y"); ?> Мій Блог. Усі права захищені.</p>
+</footer>
 </body>
 </html>
--- comments.php
+++ comments.php
@@ -1,21 +1,67 @@
-<?php include 'connections/my_site_db.php';
-$note_id = $_GET['note'];
+<?php include 'connections/my_site_db.php'; ?>
+<!DOCTYPE html>
+<html lang="uk">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Коментарі</title>
+    <link rel="stylesheet" href="styles/style.css">
+</head>
+<body>
+<header>
+    <div id="header_container">
+        <a href="blog.php" class="logo-text">Мій Блог</a>
+        <nav class="menu">
+            <ul class="menu_list">
+                <li class="menu_item"><a href="blog.php" class="menu_link">На головну</a></li>
+                <li class="menu_item"><a href="inform.html" class="menu_link">Інформація</a></li>
+            </ul>
+        </nav>
+    </div>
+</header>
 
-$query = "SELECT title, created, article FROM notes WHERE id = $note_id";
-$select_note = mysqli_query($link, $query);
-$note = mysqli_fetch_array($select_note);
-echo "<strong>{$note['title']}</strong><br>";
-echo "{$note['created']}<br>";
-echo "{$note['article']}<br><hr><hr>";
+<main>
+    <div class="main_content">
+        <?php
+        $note_id = $_GET['note'];
 
-$query_comments = "SELECT author, comment, created FROM comments WHERE art_id = $note_id ORDER BY created DESC";
-$select_comments = mysqli_query($link, $query_comments);
-if (mysqli_num_rows($select_comments) == 0) {
-    echo "Цей запис ще ніхто не коментував";
-} else {
-    while ($comment = mysqli_fetch_array($select_comments)) {
-        echo "<em>{$comment['author']}</em><br>";
-        echo "<small>{$comment['created']}</small><br>";
-        echo "{$comment['comment']}<br><hr>";
-    }
-}
+        // Виведення запису
+        $query = "SELECT title, created, article FROM notes WHERE id = $note_id";
+        $select_note = mysqli_query($link, $query);
+        $note = mysqli_fetch_array($select_note);
+
+        echo "<article class='post-card-comment'>";
+        echo "<h2 class='post-title'>{$note['title']}</h2>";
+        echo "<p class='post-meta'>Опубліковано: {$note['created']}</p>";
+        echo "<div class='post-article'>{$note['article']}</div>";
+        echo "</article>";
+        ?>
+
+        <div class="comments-block">
+            <h3 class="comments-header">Коментарі:</h3>
+            <?php
+            // Виведення коментарів
+            $query_comments = "SELECT author, comment, created FROM comments WHERE art_id = $note_id ORDER BY created DESC";
+            $select_comments = mysqli_query($link, $query_comments);
+            if (mysqli_num_rows($select_comments) == 0) {
+                echo "<p>Цей запис ще ніхто не коментував.</p>";
+            } else {
+                while ($comment = mysqli_fetch_array($select_comments)) {
+                    echo "<div class='comment-item'>";
+                    echo "<p class='comment-author'>{$comment['author']}</p>";
+                    echo "<p class='comment-meta'>{$comment['created']}</p>";
+                    echo "<p>{$comment['comment']}</p>";
+                    echo "</div>";
+                }
+            }
+            ?>
+            <a href="blog.php" class="back-to-notes-link">Повернутися до заміток</a>
+        </div>
+    </div>
+</main>
+
+<footer>
+    <p>&copy; <?php echo date("Y"); ?> Мій Блог. Усі права захищені.</p>
+</footer>
+</body>
+</html>
